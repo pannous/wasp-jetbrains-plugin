fda3d637416d1b12574a4354280e7f9971cba78a
pannous <info@pannous.com>
2025-07-20 11:34:35 +0200

    Changes complete - tests passing - 2025-07-20 11:34:35

--- src/test/kotlin/com/pannous/wasp/WaspParserTest.kt
+++ src/test/kotlin/com/pannous/wasp/WaspParserTest.kt
@@ -248,6 +248,12 @@ class WaspParserTest : ParsingTestCase("", "wasp", WaspParserDefinition()) {
         assertNotNull("Result should not be null", result)
     }
 
+    fun testBasicArithmetic() {
+        val code = "result = 10 + 20"
+        val result = parseCode(code)
+        assertNotNull("Result should not be null", result)
+    }
+
     private fun parseCode(code: String): ASTNode? {
         val psiFile = createPsiFile("test", code)
         return psiFile?.node
