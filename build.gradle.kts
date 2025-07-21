dd964c00eda6c76d63ad92001cb5553eb86f3ed5
pannous <info@pannous.com>
2025-07-21 09:02:57 +0200

    fix: add missing plugin.xml file and update gradle configuration
    The plugin was failing to load because plugin.xml was missing.
    - Created src/main/resources/META-INF/plugin.xml with proper plugin metadata
    - Updated gradle configuration to include resources directory
    - This fixes the "Fail to load plugin descriptor" error

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ repositories {
 sourceSets {
     main {
         kotlin.srcDirs("src/wasp")
+        resources.srcDirs("src/main/resources")
     }
     test {
         kotlin.srcDirs("test/wasp")
@@ -29,14 +30,14 @@ dependencies {
 
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-//    version.set("2022.2.5")
-//    type.set("IC") // Target IDE Platform
+    version.set("2022.2.5")
+    type.set("IC") // Target IDE Platform
     
     // Use a persistent sandbox directory
-//    sandboxDir.set("${rootProject.projectDir}/sandbox")
+    sandboxDir.set("${rootProject.projectDir}/sandbox")
     
     // Alternatively, use your local IDE installation
-     localPath.set("/Applications/IntelliJ IDEA.app/Contents")
+//     localPath.set("/Applications/IntelliJ IDEA.app/Contents")
 
     plugins.set(listOf(/* Plugin Dependencies */))
 }
@@ -63,7 +64,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("222")
-        untilBuild.set("252.*")
+        untilBuild.set("253.*")
     }
 
     signPlugin {
