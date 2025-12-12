5c6d95011fa7f2df503cdf67e2ebb1f6dec4b88d
pannous <info@pannous.com>
2025-12-12 11:26:38 +0100

    feat(major): update to IntelliJ Platform 2025.3
    - Upgrade gradle-intellij-platform plugin from 1.13.3 to 2.10.5
    - Update Gradle from 8.1.1 to 8.13
    - Update Kotlin from 2.1.0 to 2.1.10
    - Migrate from org.jetbrains.intellij to org.jetbrains.intellij.platform plugin
    - Upgrade Java toolchain and compilation target from 17 to 21
    - Update sinceBuild from 222 to 253 (IntelliJ IDEA 2025.3)
    - Add testFramework dependency for platform tests
    - Add .intellijPlatform to .gitignore
    - Configure Java 21 toolchain to avoid Java 25 compatibility issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,23 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
 
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "2.1.0"
-    id("org.jetbrains.intellij") version "1.13.3"
+    id("org.jetbrains.kotlin.jvm") version "2.1.10"
+    id("org.jetbrains.intellij.platform") version "2.10.5"
 }
 
 group = "com.pannous"
 version = "1.0-SNAPSHOT"
 
+kotlin {
+    jvmToolchain(21)
+}
+
 repositories {
     mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
 sourceSets {
@@ -28,32 +35,32 @@ tasks.processResources {
 }
 
 dependencies {
+    intellijPlatform {
+        local("/Applications/IntelliJ IDEA.app/Contents")
+        testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
+    }
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.opentest4j:opentest4j:1.3.0")
 }
 
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-//    version.set("2022.2.5")
-//    type.set("IC") // Target IDE Platform
-
-    // Use a persistent sandbox directory
-//    sandboxDir.set("${rootProject.projectDir}/sandbox")
-
-    // Alternatively, use your local IDE installation
-    localPath.set("/Applications/IntelliJ IDEA.app/Contents")
-
-    plugins.set(listOf(/* Plugin Dependencies */))
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "253"
+            untilBuild = provider { null }
+        }
+    }
 }
 
 tasks {
     // Set the JVM compatibility versions
     withType<JavaCompile> {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
     }
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        compilerOptions.jvmTarget.set(JVM_17)
+        compilerOptions.jvmTarget.set(JVM_21)
 
     }
 
@@ -64,11 +71,6 @@ tasks {
             showStandardStreams = true
             showStackTraces = false
         }
-    }
-
-    patchPluginXml {
-        sinceBuild.set("222")
-        untilBuild.set("253.*")
     }
 
     signPlugin {
