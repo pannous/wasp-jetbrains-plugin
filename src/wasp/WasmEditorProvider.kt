9d54c3d5a76a7010f04d598913c7e7a455034367
pannous <info@pannous.com>
2025-12-31 19:23:32 +0100

    fix: make WasmEditorProvider DumbAware for HIDE_DEFAULT_EDITOR policy
    The HIDE_DEFAULT_EDITOR policy requires providers to implement DumbAware
    interface to work during IDE indexing.

--- src/wasp/WasmEditorProvider.kt
+++ src/wasp/WasmEditorProvider.kt
@@ -4,10 +4,11 @@ import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.fileEditor.FileEditorPolicy
 import com.intellij.openapi.fileEditor.FileEditorProvider
+import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 
-class WasmEditorProvider : FileEditorProvider {
+class WasmEditorProvider : FileEditorProvider, DumbAware {
     private val LOG = Logger.getInstance(WasmEditorProvider::class.java)
 
     override fun accept(project: Project, file: VirtualFile): Boolean {
